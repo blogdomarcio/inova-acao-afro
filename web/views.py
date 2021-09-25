0cd9eee30701e5efc6b078973e8efbc29550aeaa
Marcio <blogdomarcio@live.com>
2021-09-25 15:22:07 -0300

    Fix Busca

--- web/views.py
+++ web/views.py
@@ -2,11 +2,13 @@ import json
 from datetime import date, datetime
 
 import requests
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from sweetify import sweetify
+
 from inovacao_afro.settings import ALPHA_API
 
 
@@ -28,25 +30,52 @@ def consulta(request):
 
         data = r.json()
 
+
         dia = datetime.strptime(request.POST['data'], '%Y-%m-%d').date()
+
+        try:
+
+           pesquisa = data['Time Series (Daily)'][request.POST['data']]
+
+           context = {
+
+               'data': data,
+               'dia': dia,
+               'empresa': empresa,
+               'filto': pesquisa,
+               'open': data['Time Series (Daily)'][request.POST['data']]['1. open'],
+               'high': data['Time Series (Daily)'][request.POST['data']]['2. high'],
+               'low': data['Time Series (Daily)'][request.POST['data']]['3. low'],
+               'close': data['Time Series (Daily)'][request.POST['data']]['4. close'],
+               'adjusted': data['Time Series (Daily)'][request.POST['data']]['5. adjusted close'],
+
+           }
+
+           return render(request, 'consulta.html', context)
+
+        except:
+
+            pesquisa = ''
+            sweetify.error(request, 'Data nao contem registros!',
+                           text='Tente Novamente!',
+                           persistent='Fechar')
+
+
 
         # print(request.POST['data'])
 
         context = {
 
-            'data': data,
-            'dia': dia,
-            'empresa': empresa,
-            'filto': data['Time Series (Daily)'][request.POST['data']],
-            'open': data['Time Series (Daily)'][request.POST['data']]['1. open'],
-            'high': data['Time Series (Daily)'][request.POST['data']]['2. high'],
-            'low': data['Time Series (Daily)'][request.POST['data']]['3. low'],
-            'close': data['Time Series (Daily)'][request.POST['data']]['4. close'],
-            'adjusted': data['Time Series (Daily)'][request.POST['data']]['5. adjusted close'],
+            # 'filto': pesquisa,
+            # 'open': data['Time Series (Daily)'][request.POST['data']]['1. open'],
+            # 'high': data['Time Series (Daily)'][request.POST['data']]['2. high'],
+            # 'low': data['Time Series (Daily)'][request.POST['data']]['3. low'],
+            # 'close': data['Time Series (Daily)'][request.POST['data']]['4. close'],
+            # 'adjusted': data['Time Series (Daily)'][request.POST['data']]['5. adjusted close'],
 
         }
 
-        return render(request, 'consulta.html', context)
+        return render(request, 'consulta.html')
 
     else:
 
