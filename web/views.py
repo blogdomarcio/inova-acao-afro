67b88642f462549b8046bc96c05d817ba43addbc
Marcio <blogdomarcio@live.com>
2021-09-25 15:45:25 -0300

    Fix Consulta

--- web/views.py
+++ web/views.py
@@ -15,11 +15,6 @@ from inovacao_afro.settings import ALPHA_API
 def index(request):
     return render(request, 'index.html')
 
-
-class HttpReponse(object):
-    pass
-
-
 def consulta(request):
     if request.POST:
 
@@ -28,54 +23,58 @@ def consulta(request):
         url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + empresa + "&outputsize=full&apikey=" + ALPHA_API
         r = requests.get(url)
 
-        data = r.json()
+        if r.status_code == 200:
+
+            data = r.json()
+
+            dia = datetime.strptime(request.POST['data'], '%Y-%m-%d').date()
+
+            try:
+
+               pesquisa = data['Time Series (Daily)'][request.POST['data']]
+
+               context = {
+
+                   'data': data,
+                   'dia': dia,
+                   'empresa': empresa,
+                   'filto': pesquisa,
+                   'open': data['Time Series (Daily)'][request.POST['data']]['1. open'],
+                   'high': data['Time Series (Daily)'][request.POST['data']]['2. high'],
+                   'low': data['Time Series (Daily)'][request.POST['data']]['3. low'],
+                   'close': data['Time Series (Daily)'][request.POST['data']]['4. close'],
+                   'adjusted': data['Time Series (Daily)'][request.POST['data']]['5. adjusted close'],
+
+               }
+
+               return render(request, 'consulta.html', context)
+
+            except:
+
+                pesquisa = ''
+                sweetify.error(request, 'Data não contem registros!',
+                               text='Tente Novamente!',
+                               persistent='Fechar')
 
 
-        dia = datetime.strptime(request.POST['data'], '%Y-%m-%d').date()
+            return render(request, 'consulta.html')
 
-        try:
+        elif r.status_code == 11001:
 
-           pesquisa = data['Time Series (Daily)'][request.POST['data']]
-
-           context = {
-
-               'data': data,
-               'dia': dia,
-               'empresa': empresa,
-               'filto': pesquisa,
-               'open': data['Time Series (Daily)'][request.POST['data']]['1. open'],
-               'high': data['Time Series (Daily)'][request.POST['data']]['2. high'],
-               'low': data['Time Series (Daily)'][request.POST['data']]['3. low'],
-               'close': data['Time Series (Daily)'][request.POST['data']]['4. close'],
-               'adjusted': data['Time Series (Daily)'][request.POST['data']]['5. adjusted close'],
-
-           }
-
-           return render(request, 'consulta.html', context)
-
-        except:
-
-            pesquisa = ''
-            sweetify.error(request, 'Data nao contem registros!',
-                           text='Tente Novamente!',
+            sweetify.error(request, 'Site Indisponível!',
+                           text='Tente Novamente mais tarde!',
                            persistent='Fechar')
 
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-        # print(request.POST['data'])
+        else:
 
-        context = {
+            sweetify.error(request, 'Site Indisponível!',
+                           text='Tente Novamente mais tarde!',
+                           persistent='Fechar')
 
-            # 'filto': pesquisa,
-            # 'open': data['Time Series (Daily)'][request.POST['data']]['1. open'],
-            # 'high': data['Time Series (Daily)'][request.POST['data']]['2. high'],
-            # 'low': data['Time Series (Daily)'][request.POST['data']]['3. low'],
-            # 'close': data['Time Series (Daily)'][request.POST['data']]['4. close'],
-            # 'adjusted': data['Time Series (Daily)'][request.POST['data']]['5. adjusted close'],
-
-        }
-
-        return render(request, 'consulta.html')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     else:
 
